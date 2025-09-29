5ba2afab6412c61b209050b65c1f0adbfd685bc7
AngelMaroto <angel.margar@educa.jcyl.es>
2025-09-29 11:10:35 +0200

    actualizacion base de datos

--- src/main/resources/BaseDatos/CentroMedico.sql
+++ src/main/resources/BaseDatos/CentroMedico.sql
@@ -34,11 +34,11 @@ REFERENCES Especialidad(idE);
 
 --INSERT paciente
 INSERT INTO Paciente VALUES
-(1,'71188856R','Carlos','carlos','c/Cortada n3','623845672'),
-(2,'52900473H','María','maria','Av. Libertad 45','612334567'),
-(3,'83529104L','José','jose','c/Mayor 12','634556789'),
-(4,'74219835M','Lucía','lucia','c/Colón 89','654123987'),
-(5,'61983725Z','Antonio','antonio','Plaza España 3','622789345'),
+(1,'71188856R','Carlos', SHA2('carlos',256),'c/Cortada n3','623845672'),
+(2,'52900473H','María',SHA2('maria' ,256),'Av. Libertad 45','612334567'),
+(3,'83529104L','José',SHA2('jose' ,256),'c/Mayor 12','634556789'),
+(4,'74219835M','Lucía',SHA2('lucia' ,256),'c/Colón 89','654123987'),
+(5,'61983725Z','Antonio',SHA2('antonio' ,256),'Plaza España 3','622789345'),
 (6,'98321745B','Laura','laura','c/Sevilla 77','699223344'),
 (7,'71593284F','Javier','javier','Av. Andalucía 102','633445566'),
 (8,'85219473D','Elena','elena','c/Granada 15','611778899'),
